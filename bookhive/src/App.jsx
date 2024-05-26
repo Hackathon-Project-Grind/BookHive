85c496fd123f882d0c0dc43153e2824e72e4f101
pranav-P92 <patnalapranav@gmail.com>
2024-05-26 18:08:14 +0530

    app

--- bookhive/src/App.jsx
+++ bookhive/src/App.jsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { Login, Register } from './components';
-import Home from './Pages/Home'; 
-
+import Login from './components/Login/Login';
+import Header from './components/Login/Header';
+import Register from './components/Register/Register'
 import './App.css';
+import Navbar from './components/Navigation/Navbar.jsx'
+import Home from './Pages/Home.jsx';
+import Books from './Pages/Books.jsx';
 import './index.css';
-import Navbar from './components/Navigation/Navbar';
+import Request from './Pages/Request.jsx'
+import List from './Pages/List.jsx';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <div>
-        <Routes>
-
-          <Route path="/" element={<Navigate to="/login" replace />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/home" element={<Home />} />
-          
-        </Routes>
+        <Login/>
+        <Navigation />
       </div>
     </Router>
   );
--- bookhive/src/Pages/Books.jsx
+++ bookhive/src/Pages/Books.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import './Books.css';
 
-const Books=()=>{
+const Books = () => {
+  const books = [
+    { title: 'Clean Code', author: 'Robert C. Martin', year: 2008 },
+    { title: 'You Don\'t Know JS', author: 'Kyle Simpson', year: 2015 },
+    { title: 'JavaScript: The Good Parts', author: 'Douglas Crockford', year: 2008 },
+    { title: 'Effective Java', author: 'Joshua Bloch', year: 2018 },
+    { title: 'The Pragmatic Programmer', author: 'Andrew Hunt and David Thomas', year: 1999 },
+  ];
+
   return (
-    <div className='books'>
-      <h1>Books</h1>
+    <div className="books-container">
+      {books.map((book, index) => (
+        <div className="books-card" key={index}>
+          <h2 className="books-title">{book.title}</h2>
+          <p className="books-author">Author: {book.author}</p>
+         
+          <p className="books-year">Year: {book.year}</p>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default Books;

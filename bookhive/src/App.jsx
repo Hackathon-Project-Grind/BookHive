661f2f5a8a686efefbf75e3aabcf88ea7723cbfd
Eeshwar <eesrut1@gmail.com>
2024-05-26 17:07:43 +0530

    list

--- bookhive/src/App.jsx
+++ bookhive/src/App.jsx
@@ -11,14 +11,14 @@ function App() {
     <Router>
       <div>
 
-        <Navbar />
 
         <Routes>
           <Route path="/" element={<Navigate to="/login" replace />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-    
+
           <Route path="/home" element={<Home />} />
+          
         </Routes>
       </div>
     </Router>

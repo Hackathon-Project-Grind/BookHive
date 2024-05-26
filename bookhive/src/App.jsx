b301c15a20ed2ed7ca4a2bf16e85302ae581677f
pranav-P92 <patnalapranav@gmail.com>
2024-05-26 14:31:01 +0530

    navigation

--- bookhive/src/App.jsx
+++ bookhive/src/App.jsx
@@ -1,14 +1,28 @@
 import Login from './components/Login/Login';
 import Header from './components/Login/Header';
 import Register from './components/Register/Register'
+import './App.css';
+import Navbar from './components/Navigation/Navbar.jsx'
+import Home from './Pages/Home.jsx';
+import Books from './Pages/Books.jsx';
+import './index.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-function App(){
+function App() {
   return (
-    <div>
-      <Login/>
+    <Router>
 
-    </div>
+      <div>
+      
+        <Navbar />
+        <Login/>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/books' element={<Books/>} />
+        </Routes>
+      </div>
+    </Router>
   );
-};
+}
 
 export default App;

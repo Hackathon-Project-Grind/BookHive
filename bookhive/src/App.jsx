1a778af712e64c3d752c4837e7be6921ed83c686
Eeshwar <eesrut1@gmail.com>
2024-05-26 18:15:21 +0530

    Requested

--- bookhive/src/App.jsx
+++ bookhive/src/App.jsx
@@ -1,26 +1,28 @@
-import Login from './components/Login/Login';
-import Header from './components/Login/Header';
-import Register from './components/Register/Register'
-import './App.css';
-import Navbar from './components/Navigation/Navbar.jsx'
-import Home from './Pages/Home.jsx';
-import Books from './Pages/Books.jsx';
-import './index.css';
-import Request from './Pages/Request.jsx'
-import List from './Pages/List.jsx';
-
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login/Login'; // Ensure only one import for Login
+import Register from './components/Register/Register';
+import Navbar from './components/Navigation/Navbar';
+import Home from './Pages/Home';
+import Books from './Pages/Books';
+import Request from './Pages/Request';
+import List from './Pages/List';
+
+import './App.css';
+import './index.css';
 
 function App() {
   return (
     <Router>
       <div>
+        <Navbar />
         <Routes>
-
-          <Route path="/" element={<Navigate to="/login" replace />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<Login />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/books" element={<Books />} />
+          <Route path="/request" element={<Request />} />
+          <Route path="/list" element={<List />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </div>
     </Router>
